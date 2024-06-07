b881c697af14767e07f796d7ca6fa815f962037f
JuanmanuelPereyra01 <pereyradalumosjuanma@gmail.com>
2024-06-07 15:47:50 -0300

    tercercommit

--- ecommerce-reactjs-main/src/components/ProductCard.jsx
+++ ecommerce-reactjs-main/src/components/ProductCard.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
-import './ProductCard.css';
+import React, { useState } from 'react';
+import './ProductCard.css'; // Importa el archivo CSS
 
 const ProductCard = ({ product, addToCart }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleDecrement = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleAddToCart = () => {
+    if (quantity > 0) {
+      addToCart(product, quantity);
+    }
+  };
+
   return (
     <div className="product-card">
       <img src={product.image} alt={product.title} className="product-image" />
       <div className="product-info">
-        <h3>{product.title}</h3>
-        <p>{product.description}</p>
+        <h3 className="product-title">{product.title}</h3>
+        <p className="product-description">{product.description}</p>
+        <div className="quantity-control">
+          <button onClick={handleDecrement} className="quantity-button">-</button>
+          <span className="quantity">{quantity}</span>
+          <button onClick={handleIncrement} className="quantity-button">+</button>
+        </div>
         <div className="product-footer">
           <span className="product-price">${product.price}</span>
-          <button onClick={() => addToCart(product)} className="add-to-cart-button">
+          <button onClick={handleAddToCart} className="add-to-cart-button">
             Comprar
           </button>
         </div>
--- ecommerce-reactjs-main/src/components/ProductList.jsx
+++ ecommerce-reactjs-main/src/components/ProductList.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ProductCard from './ProductCard';
-import './ProductList.css';
+import './ProductList.css'; // Importa el archivo CSS si es necesario
 
-const ProductList = ({ products, addToCart }) => {
+function ProductList({ products, addToCart }) {
   return (
     <div className="product-list">
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} addToCart={addToCart} />
+        <ProductCard key={product.title} product={product} addToCart={addToCart} />
       ))}
     </div>
   );
-};
+}
 
 export default ProductList;
